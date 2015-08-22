e9063811db766729d1fff83eb3cf3eb7fa1e1a0b
Sameer Rahmani <lxsameer@gnu.org>
2015-08-22 13:23:49 +0430

    engine name added

--- lib/rahyab_rails/engine.rb
+++ lib/rahyab_rails/engine.rb
@@ -1,6 +1,7 @@
 module RahyabRails
   class Engine < ::Rails::Engine
     isolate_namespace RahyabRails
+    engine_name 'rahyab_rails'
 
     # Map `api` to `API` in Rails autoload
     ActiveSupport::Inflector.inflections(:en) do |inflect|
