3a916f3d582b49dc94cdf6e28402cfedfd69723d
Sameer Rahmani <lxsameer@gnu.org>
2015-10-23 14:02:29 +0330

    bulk send job improved

--- app/jobs/rahyab_rails/bulk_send_job.rb
+++ app/jobs/rahyab_rails/bulk_send_job.rb
@@ -5,13 +5,22 @@ module RahyabRails
     def perform(source_number, cell_numbers, text)
       api = RahyabRails::API.new
       cell_numbers.each do |cell_number|
+
         unless cell_number.nil?
+          message = Message.new
+          message.
           Rails.logger.info "Send to: #{cell_number}"
           result = api.send_sms(source_number, [cell_number], text)
+          Rails.logger.info "Sent: #{result}"
 
-          if result != 0
+          if !result.nil?
+            message.status = :sent
+
             sleep 2
             delivery = api.get_delivery(result)
+
+            delivery = 'Failed' if delivery.nil?
+            message.status = delivery
 
             Rails.logger.info "Delivery: #{delivery}"
           # batch  = delivery["smsStatusPollResponse"]["batch"]
@@ -20,12 +29,12 @@ module RahyabRails
 
           # puts "Batch: #{result}  Status: #{status} Time: #{time}"
           else
-            f.puts "Send failed to: #{cell_number}"
-            puts "Send failed to: #{cell_number}"
-
+            Rails.logger.warning "Send failed to: #{cell_number}"
+            message.status = 'Failed to send'
           end
         end
 
+        message.save
     end
   end
 end
